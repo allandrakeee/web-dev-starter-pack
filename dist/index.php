cb7955aeb2889543d55c64adefe19a7be009d3c6
allandrakeee <allandulay69@gmail.com>
2017-10-29 18:53:40 -0800

    Added to source code.

---
+++ dist/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web Development Starter Pack</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,800">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/theme.css">
+	
+	<!-- Uncomment this if the project is ready in production -->
+	<!-- <link rel="stylesheet" href="assets/css/theme.min.css"> -->
+</head>
+<body>
+	
+	<script src="assets/js/theme.js"></script>
+
+ 	<!-- Uncomment this if the project is ready in production -->
+	<!-- <script src="assets/js/theme.min.js"></script></body> -->
+</html>
