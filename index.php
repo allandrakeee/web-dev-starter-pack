d7fb1f35f8efa26c4a2e5d9753de958579fd6fed
allandrakeee <allandulay69@gmail.com>
2017-11-05 13:28:55 -0800

    Added livereload.

--- index.php
+++ index.php
@@ -8,13 +8,16 @@
 	
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,800">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<!-- For development only | Comment or delete this line of code when its production -->
 	<link rel="stylesheet" href="assets/css/theme.css">
 	
 	<!-- Uncomment this if the project is ready in production -->
 	<!-- <link rel="stylesheet" href="assets/css/theme.min.css"> -->
 </head>
 <body>
-	
+	<h1 class="edited">Starter Pack</h1>
+	<h1 class="edited">@author Allan Drake</h1>
 	<script src="assets/js/theme.js"></script>
 
  	<!-- Uncomment this if the project is ready in production -->
